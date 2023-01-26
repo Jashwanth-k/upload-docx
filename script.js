0a46456b3096b4c04f3f5ad2bf4b5210bec51016
jashwanth <jashwanth328910@gmail.com>
2023-01-26 23:43:10 +0530

    adding formdata

--- script.js
+++ script.js
@@ -2,19 +2,28 @@ const submitForm = (e) => {
   e.preventDefault();
   data = [];
   let rname = document.getElementById("name").value;
-  let img = document.getElementById("img").value;
-  let script = document.getElementById("script").value;
+  // debugger;
+  let img = document.getElementById("img").files[0];
+  let script = document.getElementById("script").files[0];
   let price = document.getElementById("price").value;
+  let audio = document.getElementById("music").files[0];
   /* saveData(rname, img, script, price); */
-  let newData = {
-    rname: rname,
-    img: img,
-    script: script,
-    price: price,
-  };
-  data.push(newData);
 
-  console.log(data);
+  form = new FormData();
+  form.append("name", rname);
+  form.append("image", img);
+  form.append("script", script);
+  form.append("price", price);
+  form.append("audio", audio);
+
+  // console.log(form);
+  fetch("http://localhost:8081/image/", {
+    method: "POST",
+    body: form,
+  })
+    .then((data) => data.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
 };
 /* const saveData = (rname, img, script, price) => {
  
